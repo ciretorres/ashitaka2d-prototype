1138091a27c97e6a6e26e0e7b224cbed12b806ef
ciretorres <erictorres.velasco@gmail.com>
2023-10-27 02:07:15 -0600

    making the transition trigger to other scenes with new tiled maps and collisions

---
+++ assets/art/Forest Spirit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Forest Spirit" tilewidth="8" tileheight="8" tilecount="64" columns="16">
+ <image source="../../../../Unity/Ashitaka2D/Assets/Sprites/Forest Spirit.png" width="128" height="32"/>
+</tileset>
